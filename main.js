c8d5fc44ff8e2af7590ebf00eee01b57a9b8c763
sethv76 <bob.gone.bad@gmail.com>
2015-09-06 14:32:24 -0400

    yo

---
+++ main.js
@@ -0,0 +1,46 @@
+var cookies = 0;
+var ticker = [];
+
+startUp();
+
+
+function startUp() {
+	addTicker("Don't click the button.");
+}
+
+function cookiesClick() {
+	cookies += 1;
+	document.getElementById("cookies").innerHTML = cookies;
+	checkClicks();
+}
+
+
+function checkClicks() {
+	switch(cookies) {
+		case 1:
+			addTicker("You're a cheaky little fucker aren't you.");
+			break;
+
+		default:
+			break;
+	}
+
+}
+
+
+function addTicker(tick) {
+	//adds to the ticker
+	if(ticker.length > 10){
+		ticker.pop();
+	}
+
+	ticker.unshift(tick);
+	
+
+	//Prints to the ticker
+	document.getElementById("ticker").innerHTML = '';
+
+	ticker.forEach(function (line, index, ticker) {
+		document.getElementById("ticker").innerHTML += line + "<br><br>";
+	});
+}
